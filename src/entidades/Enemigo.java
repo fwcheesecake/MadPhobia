b3c8af6875f6aebff343e7ceb9d7b609079ca422
Danfeik <segura.tinajero.19121@itsmante.edu.mx>
2021-04-04 19:03:56 -0500

    Agrego clase ser

--- src/entidades/Enemigo.java
+++ src/entidades/Enemigo.java
@@ -1,4 +1,27 @@
 package entidades;
 
-public class Enemigo {
+
+import enumerados.Tipo;
+
+public class Enemigo extends Ser {
+    protected Tipo fortaleza;
+    protected Tipo debilidad;
+    public Enemigo() {
+        super();
+        setFortaleza(Tipo.MELEE);
+        setDebilidad(Tipo.MELEE);
+    }
+    public Tipo getFortaleza() {
+        return fortaleza;
+    }
+    public void setFortaleza(Tipo fortaleza) {
+        this.fortaleza = fortaleza;
+    }
+    public Tipo getDebilidad() {
+        return debilidad;
+    }
+    public void setDebilidad(Tipo debilidad) {
+        this.debilidad = debilidad;
+    }
+
 }
---
+++ src/entidades/Ser.java
@@ -0,0 +1,8 @@
+package entidades;
+
+public class Ser extends Entidad{
+    protected int vida;
+    protected int escudo;
+    protected int fuerza;
+
+}
